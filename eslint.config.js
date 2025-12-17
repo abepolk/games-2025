8827a3b739ab2f18627d1ef6646737ad6e397707
Abe Polk <21989062+abepolk@users.noreply.github.com>
2025-12-17 05:02:52 -0500

    Set up Eslint Stylistic with some presets based on what we're already doing

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,18 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
+      stylistic.configs.customize({
+        semi: true,
+        commaDangle: 'never',
+        quotes: 'double',
+      }),
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
@@ -22,8 +28,12 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  
 ])
