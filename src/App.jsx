529de76f2b38f6bcc643c6d364f98ce2f3de9f47
Abe Polk <21989062+abepolk@users.noreply.github.com>
2026-01-04 11:44:57 -0500

    Add SVG placeholder for weapon icons

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,10 @@ const HealthBar = ({ attackable, current, max, label, color, index, handleAction
         />
       </div>
     </div>
+    {/* Placeholder for dagger, stick and spear icons */}
+    <svg class="h-10 ml-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
   </div>
 );
 
