59fe0dc68498b572bc71c092298e443bd31ef11a
Parker Porfilio <530116+pporfilio@users.noreply.github.com>
2026-03-10 17:46:23 -0400

    Added test that checks for a thrown error.

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -5,3 +5,7 @@ test("Selects random element from [1, 2, 3]", () => {
   const testArray = [1, 2, 3];
   expect(selectRandomElement(testArray)).toBeOneOf(testArray);
 });
+
+test("Throws an error when given an empty array", () => {
+  expect(() => selectRandomElement([])).toThrowError("empty array");
+});
